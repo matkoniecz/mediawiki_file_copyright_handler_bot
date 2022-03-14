3a456f04907748f0463db24c4003f4593030fe40
Mateusz Konieczny <matkoniecz@gmail.com>
2022-03-14 09:54:50 +0100

    change incoming sources

--- edit_about_missing_file_source_or_license.py
+++ edit_about_missing_file_source_or_license.py
@@ -58,9 +58,11 @@ def main():
     #shared.pause()
     
     sources = []
-    sources.append({"description": "has {{unknown}}", "files": mediawiki_api_query.pages_from_category("Category:Media without a license - without subcategory")})
+    #sources.append({"description": "has {{unknown}}", "files": mediawiki_api_query.pages_from_category("Category:Media without a license - without subcategory")}) # all viable files processed
     sources.append({"description": "without any category", "files": uncategorized_images_skipping_some_initial_ones()})
-    sources.append({"description": "by date", "files": mediawiki_api_query.images_by_date("2008-01-01T18:05:46Z")})
+    sources.append({"description": "by date, from 2008", "files": mediawiki_api_query.images_by_date("2008-01-01T18:05:46Z")})
+    sources.append({"description": "by date, from 2000", "files": mediawiki_api_query.images_by_date("2000-01-01T18:05:46Z")})
+    sources.append({"description": "by date, from 2015", "files": mediawiki_api_query.images_by_date("2015-01-01T18:05:46Z")})
     sources.append({"description": "by date, from 2022", "files": mediawiki_api_query.images_by_date("2022-01-01T00:00:00Z")})
     random.shuffle(sources)
     for source in sources:
