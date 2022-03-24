8b159491d8fe68836a0db0de04973bf0f8b3938b
Mateusz Konieczny <matkoniecz@gmail.com>
2022-03-24 09:49:48 +0100

    add null edit for all pages

--- null_edit.py
+++ null_edit.py
@@ -10,6 +10,10 @@ def main():
     selftest()
     session = shared.create_login_session()
 
+    for page_title in mediawiki_api_query.all_pages():
+        print(page_title)
+        shared.null_edit(session, page_title)
+
     for page_title in mediawiki_api_query.pages_from_category("Category:Disabled"):
         print(page_title)
         shared.null_edit(session, page_title)
