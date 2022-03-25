1c67b52e9a64fd01ceb34014a5afe72a31a2e53e
Mateusz Konieczny <matkoniecz@gmail.com>
2022-03-25 09:23:41 +0100

    allow using specified login data

--- password_data.py
+++ password_data.py
@@ -1,18 +1,18 @@
 import json
 
-def api_login_data():
+def api_login_data(index = 'api_password'):
     with open('secret.json') as f:
         data = json.load(f)
-        username = data['api_password']['username']
-        password = data['api_password']['password']
+        username = data[index]['username']
+        password = data[index]['password']
         return {'user': username, 'password': password}
     # https://wiki.openstreetmap.org/wiki/Special:BotPasswords
 
-def username():
+def username(index = 'api_password'):
     # https://en.wikipedia.org/wiki/Wikipedia:Naming_conventions_(technical_restrictions)#Restrictions_on_usernames
     with open('secret.json') as f:
         data = json.load(f)
-        username = data['api_password']['username']
+        username = data[index]['username']
         if "@" in username:
             return username.split("@")[0]
         else:
