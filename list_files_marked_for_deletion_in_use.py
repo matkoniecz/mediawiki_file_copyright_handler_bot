60c456f121f3379f3c96284019e2b586cda295cf
Mateusz Konieczny <matkoniecz@gmail.com>
2022-05-26 17:37:56 +0200

    delete list_files_marked_for_deletion_in_use.py

--- list_files_marked_for_deletion_in_use.py
+++
@@ -1,8 +0,0 @@
-import mediawiki_api_query
-import shared
-
-for page_title in mediawiki_api_query.pages_from_category("Category:Labelled for deletion"):
-    if page_title.find("File:") != 0:
-        continue
-    if mediawiki_api_query.is_used_as_image_anywhere(page_title):
-        print("* [[:" + page_title + "]]")
